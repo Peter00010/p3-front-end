c49e614488ca79df20c63c5bb144fbc2fdb7ec5e
Peter00010 <mwongela.oboe@gmail.com>
2024-03-20 08:47:16 +0300

    Made the templates

---
+++ colors.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-unused-vars */
+const inikaFont = <style>
+@import url('https://fonts.googleapis.com/css2?family=Inika:wght@400;700&display=swap')
+</style>
+
+const josefinSlabFont = <style>
+@import url('https://fonts.googleapis.com/css2?family=Inika:wght@400;700&family=Josefin+Slab:ital,wght@0,100..700;1,100..700&display=swap')
+</style>
+
+//josefin slab font has been used to make the logo only
+//inika font has been used as the main text font 
---
+++ fonts.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+//dark mode
+const mainbg = "1F1F1F"
+const darkContainers = "171717"
+const ourPurple = "3A55EB"
+const ourWhite = "FFFFFF"
+
+//lightmode
+
+const mainBg = "F7F8FD"
